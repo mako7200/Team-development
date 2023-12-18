6377fc394da771ed08bf33d4cbfe1e844c363631
akadoh4869 <hodakobe@gmail.com>
2023-12-18 15:28:16 +0900

    編集ページ形少し作りました

---
+++ resources/views/showedit.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>プロフィール編集ページ</title>
+</head>
+<body>
+    <main>
+        <div class="cancel-btn">
+            <a href="">キャンセル</a>
+        </div>
+        <form action="" methid="">
+            @csrf
+            <div class="edit-form">
+                <div class="title-edit">
+                    <input type="text" class="form-control" placeholder="タイトルを入力して下さい" name="title" 
+                    {{-- value="{{ $todo->title }}" --}}
+                    >
+                </div>
+                <div class="content-edit">
+                    <textarea class="form-control" placeholder="内容" rows="5" name="content">
+                        {{-- {{ $todo->content }} --}}
+                    </textarea>
+
+                </div>
+                <div class="image-edit">
+                    <input id="image" type="file" class="form-control" name="image">
+                    {{-- value="{{ $todo->image }}" onchange="previewImage(this)"> --}}
+                    
+                </div>
+                <div class="upload-btn">
+                    <button type="submit" class="btn btn-primary">編集</button>
+                </div>
+
+                
+
+
+            </div>
+        </form>
+    </main>
+    
+</body>
+</html>
