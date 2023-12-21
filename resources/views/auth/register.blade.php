b2643f10f93dc328544677e68a0f11b2dd621611
Yuyadesu1020 <Puchiyongzai@gmail.com>
2023-12-21 23:28:26 +0800

    regiter.css12/21
    写真の反映だけしてない

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,129 +1,223 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app') --}}
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+{{-- @section('content') --}}
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-                        @csrf
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                        {{-- ユーザーネーム --}}
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.5.1/css/all.css">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                    <!-- Right Side Of Navbar -->
+                    {{-- <ul class="navbar-nav ms-auto"> --}}
+                        <!-- Authentication Links -->
+                        {{-- @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul> --}}
+                <div class="app-name">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button> --}}
+    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav me-auto">
+    
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div>
+                        <a href="{{ url('/') }}" class="close"><i class="fa-solid fa-xmark" style="font-size: 60px"></i></a>
+                    </div>
+                    <div class="card">
+                        {{-- <div class="card-header">{{ __('Register') }}</div> --}}
+
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+                                @csrf
+
+                                <div class="info-box">
+                                    <div class="one-block">
+                                        <div class="sub-block">
+                                            {{-- ユーザーネーム --}}
+                                            <div class="row mb-3">
+                                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Username (ユーザーネーム)※') }}</label>
+                                                <div class="col-md-6">
+                                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                                    @error('name')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            {{-- メールアドレス --}}
+                                            <div class="row mb-3">
+                                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Mail Address (メールアドレス)※') }}</label>
+                                                <div class="col-md-6">
+                                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                                    @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            {{-- パスワード --}}
+                                            <div class="row mb-3">
+                                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password (パスワード)※') }}</label>
+                                                <div class="col-md-6">
+                                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                                    @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        {{-- ここに画像選択後に反映されるimage置いてください！！  classはmyavatarで--}}
+
+
+                                    </div>
+
+                                    <div class="second-block">
+                                        {{-- パスワード確認 --}}
+                                        <div class="row mb-3">
+                                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password (パスワード確認)※') }}</label>
+                                            <div class="col-md-6">
+                                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                            </div>
+                                        </div>
+
+                                        {{-- アバター --}}
+                                        <div>
+                                            <div class="camera">
+                                            <label for="avatar" class="image"><i class="fa-solid fa-camera" style=""></i></label>
+                                            {{-- {{ __('プロフィール画像（サイズは1024Kbyteまで）') }} --}}
+
+                                            <div class="col-md-6">
+                                                <input id="avatar" type="file" class="@error('avatar') is-invalid @enderror" name="avatar" style="display: none">
+
+                                                @error('avatar')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="third-block">
+                                        {{-- 国タグ --}}
+                                        <div class="row mb-3">
+                                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Country (国名)※') }}</label>
+                                            <div class="col-md-6">
+                                                <select class="form-select" id="countries" name="country_id" >
+                                                    <option>タグを選択してください</option>
+                                                    @foreach($countries as $country)
+                                                    <option value="{{ $country->id }}">{{ $country->country_name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        
+                                        {{-- 職種タグ --}}
+                                        <div class="row mb-3">
+                                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Occupation (職業)※') }}</label>
+                                            <div class="col-md-6">
+                                                <select class="form-select" id="occupations" name="occupation_id" >
+                                                    <option>タグを選択してください</option>
+                                                    @foreach($occupations as $occupation)
+                                                    <option value="{{ $occupation->id }}">{{ $occupation->occupation_name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {{-- 登録ボタン --}}
+                                <div class="submit-box">
+                                    <div class="btn-box">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('登録') }}
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </form>
                         </div>
-                        
-                        {{-- アバター --}}
-                        <div class="row mb-3">
-                            <label for="avatar" class="col-md-4 col-form-label text-md-end">{{ __('プロフィール画像（サイズは1024Kbyteまで）') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="avatar" type="file" class="@error('avatar') is-invalid @enderror" name="avatar">
-
-                                @error('avatar')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- 国タグ --}}
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Country') }}</label>
-                            <div class="col-md-6">
-                                <select class="form-select" id="countries" name="country_id" >
-                                    <option>タグを選択してください</option>
-                                    @foreach($countries as $country)
-                                    <option value="{{ $country->id }}">{{ $country->country_name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                        
-                        {{-- 職種タグ --}}
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Occupation') }}</label>
-                            <div class="col-md-6">
-                                <select class="form-select" id="occupations" name="occupation_id" >
-                                    <option>タグを選択してください</option>
-                                    @foreach($occupations as $occupation)
-                                    <option value="{{ $occupation->id }}">{{ $occupation->occupation_name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        {{-- メールアドレス --}}
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- パスワード --}}
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- パスワード確認 --}}
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        {{-- 登録ボタン --}}
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+{{-- @endsection --}}
     </div>
-</div>
-@endsection
-
-
-
-
-
-<div>
-    <label for="avatar"><i class="fa-solid fa-camera"></i></label>
-    <input type="file" id="avatar" style="display: none">
-</div>
-<div><img src="../img/seedtech70期生徒.jpg" alt="" class="myavatar"></div>
+</body>
+</html>
 
 
