a2b7e1f2e42c0412b17480748e7101a1e2071633
Yuyadesu1020 <Puchiyongzai@gmail.com>
2023-12-27 14:08:33 +0800

    create.edit

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,25 +9,30 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.5.1/css/all.css">
 </head>
 <body>
-    <div><a href="{{ route('posts.index') }}"><i class="fa-solid fa-xmark"></i></a></div>
-    
-    <form action="{{ route('posts.store') }}" method="POST">
-        @csrf
-        <div>
-            <div>
-                <input type="text" id="title" class="titlebox">
-                <!-- <div><i class="fa-regular fa-image"></i></div> -->
-                <div>
-                    <label for="image"><i class="fa-regular fa-image"></i></label>
-                    <input type="file" id="image" style="display: none;">
+    <header>
+        <h1>アプリ名</h1>
+    </header>
+    <main>
+        <div><a href="{{ route('posts.index') }}"><i class="fa-solid fa-xmark"></i></a></div>
+        
+        <form action="{{ route('posts.store') }}" method="POST">
+            @csrf
+            <div class="createpage">
+                <div class="box1">
+                    <input type="text" id="title" class="titlebox">
+                    <!-- <div><i class="fa-regular fa-image"></i></div> -->
+                    <div>
+                        <label for="image"><i class="fa-regular fa-image"></i></label>
+                        <input type="file" id="image" style="display: none;">
+                    </div>
                 </div>
+                <div>
+                    <input type="textarea">
+                    <div><img src="../img/jobsample.jpg" alt="" class="postimage"></div>
+                </div>
+                <div><button type="submit">共有</button></div>
             </div>
-            <div>
-                <input type="textarea">
-                <div><img src="../img/jobsample.jpg" alt="" class="postimage"></div>
-            </div>
-            <div><button type="submit">共有</button></div>
-        </div>
-    </form>
+        </form>
+    </main>
 </body>
 </html>
