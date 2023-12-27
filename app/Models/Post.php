1bb7f95d7e0b3ee593b8e21fd686cc70c3ec2994
mako7200 <k168sa@gmail.com>
2023-12-28 02:23:02 +0800

    コメント機能のリレーション付け足し、微調整

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,7 +26,8 @@ class Post extends Model
         return $this->belongsTo('App\Models\Occupation');
     }
 
-    function likes()    //likeモデルとのリレーション
+    //Likeモデルとのリレーション
+    function likes()
     {
         return $this->hasMany('App\Models\Like');
     }
@@ -34,5 +35,11 @@ class Post extends Model
     public function likedBy($user)      //指定されたユーザーが指定された投稿にいいねをしているかどうかを確認するための Eloquent クエリを返すメソッド
     {
         return Like::where('user_id', $user->id)->where('post_id', $this->id);
+    }
+
+    //Commentモデルとのリレーション
+    function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
     }
 }
