7838caee23b6c0fcfcd5388bd7a3d59e714247a9
mako7200 <k168sa@gmail.com>
2024-01-03 16:01:47 +0800

    editでタグを引き継ぐ

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.5.1/css/all.css">
-    <script src="{{ asset('js/edit.js') }}"></script>   <!--✅edit.jsの読み込み-->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>   <!--✅jQueryの読み込み-->
+    <script src="{{ asset('js/edit.js') }}"></script>   <!--edit.jsの読み込み-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>   <!--jQueryの読み込み-->
 </head>
 <body>
     <header>
@@ -31,7 +31,7 @@
                         <input type="text" class="titlebox" value="{{ $post->title }}" name="title">
 
                         <div>
-                            {{-- ✅画像の入れ込み --}}
+                            {{-- 画像の入れ込み --}}
                             <label for="image"><i class="fa-regular fa-image image"></i></label>
                             <input id="image" type="file" class="form-control" name="image" value="{{ $post->image }}" onchange="previewImage(this)" style="display: none">
                         </div>
@@ -51,19 +51,24 @@
                     <div class="box3">
                         <div>
                             <label for="countries">国タグ:</label>
-                            <select class="form-select choose" id="countries" name="country_id" >
+                            <select class="form-select choose" id="countries" name="country_id">
                                 <option>タグを選択してください</option>
                                 @foreach($countries as $country)
-                                <option value="{{ $country->id }}">{{ $country->country_name }}</option>
+                                    <option value="{{ $country->id }}" @if($country->id == $selectedCountry) selected @endif>   <!--編集前のタグを引き継ぐ-->
+                                        {{ $country->country_name }}
+                                    </option>
                                 @endforeach
                             </select>
+
                         </div>
                         <div>
                             <label for="occupations">企業タグ:</label>
-                            <select class="form-select choose" id="occupations" name="occupation_id" >
+                            <select class="form-select choose" id="occupations" name="occupation_id">
                                 <option>タグを選択してください</option>
                                 @foreach($occupations as $occupation)
-                                <option value="{{ $occupation->id }}">{{ $occupation->occupation_name }}</option>
+                                    <option value="{{ $occupation->id }}" @if($occupation->id == $selectedOccupation) selected @endif>   <!--編集前のタグを引き継ぐ-->
+                                        {{ $occupation->occupation_name }}
+                                    </option>
                                 @endforeach
                             </select>
                         </div>
@@ -76,7 +81,7 @@
             </div>
         </form>
     </main>
-    {{-- ✅ライトボックスの導入（画像表示） --}}
+    {{-- ライトボックスの導入（画像表示） --}}
 <script src="{{ asset ('js/lightbox-plus-jquery.js') }}"></script>
 <script>
     lightbox.option({
