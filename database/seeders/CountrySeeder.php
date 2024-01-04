7cec28864699eedd8b57e9d9cf87669474945ed2
yukke0803 <tiger5588@docomo.ne.jp>
2024-01-04 16:39:11 +0800

    たぐ

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -22,12 +22,127 @@ class CountrySeeder extends Seeder
             [
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => null,
+                'country_name'=> 'イギリス',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'イタリア',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'オーストラリア',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'オランダ',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'カナダ',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
                 'country_name'=> '韓国',
             ],
             [
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => null,
-                'country_name'=> '日本',
+                'country_name'=> 'スイス',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'スウェーデン',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'スペイン',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'シンガポール',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> '台湾',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> '中国',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'ドイツ',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'ニュージーランド',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'フィリピン',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'フィンランド',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'フランス',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'ベルギー',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> '香港',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'アジア',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'アフリカ',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> '中東',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> '北アメリカ',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> '南アメリカ',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'country_name'=> 'ヨーロッパ',
             ],
         ]);
     }
--- database/seeders/OccupationSeeder.php
+++ database/seeders/OccupationSeeder.php
@@ -22,12 +22,7 @@ class OccupationSeeder extends Seeder
             [
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => null,
-                'occupation_name'=> 'IT・通信',
-            ],
-            [
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => null,
-                'occupation_name'=> '公務員',
+                'occupation_name'=> '金融',
             ],
             [
                 'created_at' => date('Y-m-d H:i:s'),
@@ -38,6 +33,61 @@ class OccupationSeeder extends Seeder
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => null,
                 'occupation_name'=> '商社',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> 'IT・通信',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> '広告・マスコミ',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> '人材・教育',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> 'インフラ・交通',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> '不動産・建設',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> '旅行・観光',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> 'ブライダル・美容・くらし',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> '医療福祉',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> '公務員',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> '流通・商売',
+            ],
+            [
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+                'occupation_name'=> 'その他',
             ],
         ]);
     }
